62d2859ee5783c6f05ea17f76bf6dcab45bf08ca
Luke Yeh <lukeforwardto@gmail.com>
2026-05-08 19:01:14 +0800

    feat: add route for Test component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./Home.tsx";
 import Callback from "./Callback.tsx";
+import Test from "./Test.tsx";
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/callback" element={<Callback />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );

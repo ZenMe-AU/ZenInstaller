fc738b713fd6bd3dd25e82022a657c9f0a9bee2c
Luke Yeh <175194915+luke-yeh@users.noreply.github.com>
2026-06-02 16:38:54 +0800

    docs: initialize VitePress documentation site

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  title: "ZenInstaller Docs",
+  description: "ZenInstaller documentation and guides",
+  base: "/docs/",
+
+  themeConfig: {
+    nav: [
+      { text: "App", link: "/../" },
+      { text: "Docs", link: "/" },
+    ],
+    sidebar: [
+      {
+        text: "Guides",
+        items: [
+          {
+            text: "Creating org Microsoft account",
+            link: "/Creating_org_Microsoft_account",
+          },
+        ],
+      },
+    ],
+    socialLinks: [],
+    outline: {
+      level: [2, 3],
+    },
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,12 @@
+---
+layout: home
+
+hero:
+  name: ZenInstaller
+  text: Documentation
+  tagline: Guides and references for setting up your environment
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /Creating_org_Microsoft_account
+---
